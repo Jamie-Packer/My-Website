ff35832bfe10e310a34cb0918db4501bf8bb031c
Jamie-Packer <JamieJPacker@gmail.com>
2025-12-31 15:01:18 +0000

    Remove unused loading component and fix typo
    Deleted src/app/projects/loading.tsx as it is no longer needed. Also corrected typos in university.mdx and updated a comment in .gitignore.

--- src/app/projects/loading.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/loading.tsx
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-background">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-background2 border-t-accent"></div>
-    </div>
-  );
-}
